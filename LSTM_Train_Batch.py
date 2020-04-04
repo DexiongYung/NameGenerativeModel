a8258b02b23bb3c34fedec6adb0107793ff48055
Dylan Yung <YungDexiong@hotmail.com>
2020-04-04 02:25:00 -0700

    made LSTM uniform

--- LSTM_Train_Batch.py
+++ LSTM_Train_Batch.py
@@ -9,23 +9,33 @@ import pandas as pd
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from Datasets.NameCateDL import NameCategoricalDataLoader
+from Datasets.NameDataset import NameDataset
 from Models.Decoder import Decoder
 from Utilities.Convert import *
 from Utilities.Utilities import plot_losses
 
 # Optional command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', help='Name of the Session', nargs='?', default='first', type=str)
-parser.add_argument('--hidden_size', help='Size of the hidden layer of LSTM', nargs='?', default=256, type=int)
-parser.add_argument('--embed_dim', help='Size of embedding dimension', nargs='?', default=32, type=int)
-parser.add_argument('--lr', help='Learning rate', nargs='?', default=0.0005, type=float)
-parser.add_argument('--num_iter', help='Number of iterations', nargs='?', default=200000, type=int)
-parser.add_argument('--num_layers', help='Number of layers', nargs='?', default=5, type=int)
-parser.add_argument('--train_file', help='File to train on', nargs='?', default='Data/FirstNames.csv', type=str)
-parser.add_argument('--column', help='Column header of data', nargs='?', default='name', type=str)
-parser.add_argument('--print', help='Print every', nargs='?', default=50, type=int)
-parser.add_argument('--batch', help='Batch size', nargs='?', default=5000, type=int)
+parser.add_argument('--name', help='Name of the Session',
+                    nargs='?', default='first', type=str)
+parser.add_argument('--hidden_size', help='Size of the hidden layer of LSTM',
+                    nargs='?', default=256, type=int)
+parser.add_argument('--embed_dim', help='Size of embedding dimension',
+                    nargs='?', default=32, type=int)
+parser.add_argument('--lr', help='Learning rate',
+                    nargs='?', default=0.0005, type=float)
+parser.add_argument('--num_iter', help='Number of iterations',
+                    nargs='?', default=200000, type=int)
+parser.add_argument('--num_layers', help='Number of layers',
+                    nargs='?', default=5, type=int)
+parser.add_argument('--train_file', help='File to train on',
+                    nargs='?', default='Data/FirstNames.csv', type=str)
+parser.add_argument('--column', help='Column header of data',
+                    nargs='?', default='name', type=str)
+parser.add_argument('--print', help='Print every',
+                    nargs='?', default=50, type=int)
+parser.add_argument('--batch', help='Batch size',
+                    nargs='?', default=5000, type=int)
 parser.add_argument('--continue_training', help='Boolean whether to continue training an existing model', nargs='?',
                     default=1, type=int)
 
@@ -47,7 +57,8 @@ CLIP = 1
 SOS = 'SOS'
 PAD = 'PAD'
 EOS = 'EOS'
-IN_CHARS = [char for char in string.ascii_letters] + ['\'', '-'] + [EOS, SOS, PAD]
+IN_CHARS = [char for char in string.ascii_letters] + \
+    ['\'', '-'] + [EOS, SOS, PAD]
 IN_COUNT = len(IN_CHARS)
 OUT_CHARS = [char for char in string.ascii_letters] + ['\'', '-'] + [EOS, PAD]
 OUT_COUNT = len(OUT_CHARS)
@@ -62,8 +73,10 @@ def train(x: list):
     batch_sz = len(x)
     max_len = len(max(x, key=len)) + 1  # +1 for EOS xor SOS
 
-    src_x = list(map(lambda s: [SOS] + [char for char in s] + [PAD] * ((max_len - len(s)) - 1), x))
-    trg_x = list(map(lambda s: [char for char in s] + [EOS] + [PAD] * ((max_len - len(s)) - 1), x))
+    src_x = list(
+        map(lambda s: [SOS] + [char for char in s] + [PAD] * ((max_len - len(s)) - 1), x))
+    trg_x = list(map(lambda s: [char for char in s] +
+                     [EOS] + [PAD] * ((max_len - len(s)) - 1), x))
 
     src = indexTensor(src_x, max_len, IN_CHARS).to(DEVICE)
     trg = targetsTensor(trg_x, max_len, OUT_CHARS).to(DEVICE)
@@ -89,21 +102,25 @@ def train(x: list):
     return names, loss.item()
 
 
-def iter_train(dl: NameCategoricalDataLoader, iterations: int = ITER, path: str = "Checkpoints/",
+def iter_train(dl: DataLoader, iterations: int = ITER, path: str = "Checkpoints/",
                print_every: int = PRINTS):
     all_losses = []
     total_loss = 0
+    count = 0
 
     for iter in range(1, iterations + 1):
-        x = dl.sample()
-        name, loss = train(x)
-        total_loss += loss
+        for x in dl:
+            count = count + 1
+            name, loss = train(x)
+            total_loss += loss
 
-        if iter % print_every == 0:
-            all_losses.append(total_loss / print_every)
-            total_loss = 0
-            plot_losses(all_losses, x_label=f"Iteration of Batch Size: {BATCH_SZ}", y_label="NLLosss", filename=NAME)
-            torch.save({'weights': lstm.state_dict()}, os.path.join(f"{path}{NAME}.path.tar"))
+            if count % print_every == 0:
+                all_losses.append(total_loss / print_every)
+                total_loss = 0
+                plot_losses(
+                    all_losses, x_label=f"Iteration of Batch Size: {BATCH_SZ}", y_label="NLLosss", filename=NAME)
+                torch.save({'weights': lstm.state_dict()},
+                           os.path.join(f"{path}{NAME}.path.tar"))
 
 
 def iter_train_dl(dl: DataLoader, epochs: int = ITER, path: str = "Checkpoints/", print_every: int = PRINTS):
@@ -120,7 +137,8 @@ def iter_train_dl(dl: DataLoader, epochs: int = ITER, path: str = "Checkpoints/"
                 total_loss = 0
                 plot_losses(all_losses, x_label=f"Iteration of Batch Size: {BATCH_SZ}", y_label="NLLosss",
                             filename=NAME)
-                torch.save({'weights': lstm.state_dict()}, os.path.join(f"{path}{NAME}.path.tar"))
+                torch.save({'weights': lstm.state_dict()},
+                           os.path.join(f"{path}{NAME}.path.tar"))
 
 
 def sample(length: list):
@@ -134,9 +152,11 @@ def sample(length: list):
         char = SOS
 
         for i in range(max_length):
-            lstm_probs, lstm_hidden = lstm(lstm_input[0], lng_input, lstm_hidden)
+            lstm_probs, lstm_hidden = lstm(
+                lstm_input[0], lng_input, lstm_hidden)
             lstm_probs = torch.softmax(lstm_probs, dim=2)
-            sample = torch.distributions.categorical.Categorical(lstm_probs).sample()
+            sample = torch.distributions.categorical.Categorical(
+                lstm_probs).sample()
             sample = sample[0]
             char = OUT_CHARS[sample]
 
@@ -187,5 +207,6 @@ criterion = nn.NLLLoss(ignore_index=OUT_CHARS.index(PAD))
 optimizer = torch.optim.Adam(lstm.parameters(), lr=LR)
 
 df = pd.read_csv(TRAIN_FILE)
-dl = NameCategoricalDataLoader(df, batch_sz=BATCH_SZ)
+ds = NameDataset(df, 'name')
+dl = DataLoader(ds, batch_size=2000, shuffle=True)
 iter_train(dl)
--- Uniform.py
+++ Uniform.py
@@ -16,18 +16,28 @@ from Utilities.Utilities import plot_losses
 
 # Optional command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', help='Name of the Session', nargs='?', default='first_uniform', type=str)
-parser.add_argument('--hidden_size', help='Size of the hidden layer of LSTM', nargs='?', default=256, type=int)
-parser.add_argument('--embed_dim', help='Size of embedding dimension', nargs='?', default=32, type=int)
-parser.add_argument('--lr', help='Learning rate', nargs='?', default=0.0005, type=float)
-parser.add_argument('--epoch', help='Number of epochs', nargs='?', default=200000, type=int)
-parser.add_argument('--num_layers', help='Number of layers', nargs='?', default=5, type=int)
-parser.add_argument('--train_file', help='File to train on', nargs='?', default='Data/FirstNames.csv', type=str)
-parser.add_argument('--column', help='Column header of data', nargs='?', default='name', type=str)
-parser.add_argument('--print', help='Print every', nargs='?', default=100, type=int)
-parser.add_argument('--batch', help='Batch size', nargs='?', default=250, type=int)
+parser.add_argument('--name', help='Name of the Session',
+                    nargs='?', default='first_uniform', type=str)
+parser.add_argument('--hidden_size', help='Size of the hidden layer of LSTM',
+                    nargs='?', default=256, type=int)
+parser.add_argument('--embed_dim', help='Size of embedding dimension',
+                    nargs='?', default=32, type=int)
+parser.add_argument('--lr', help='Learning rate',
+                    nargs='?', default=0.0005, type=float)
+parser.add_argument('--epoch', help='Number of epochs',
+                    nargs='?', default=200000, type=int)
+parser.add_argument('--num_layers', help='Number of layers',
+                    nargs='?', default=5, type=int)
+parser.add_argument('--train_file', help='File to train on',
+                    nargs='?', default='Data/FirstNames.csv', type=str)
+parser.add_argument('--column', help='Column header of data',
+                    nargs='?', default='name', type=str)
+parser.add_argument('--print', help='Print every',
+                    nargs='?', default=100, type=int)
+parser.add_argument('--batch', help='Batch size',
+                    nargs='?', default=250, type=int)
 parser.add_argument('--continue_training', help='Boolean whether to continue training an existing model', nargs='?',
-                    default=0, type=int)
+                    default=1, type=int)
 
 # Parse optional args from command line and save the configurations into a JSON file
 args = parser.parse_args()
@@ -47,7 +57,8 @@ CLIP = 1
 SOS = 'SOS'
 PAD = 'PAD'
 EOS = 'EOS'
-IN_CHARS = [char for char in string.ascii_letters] + ['\'', '-'] + [EOS, SOS, PAD]
+IN_CHARS = [char for char in string.ascii_letters] + \
+    ['\'', '-'] + [EOS, SOS, PAD]
 IN_COUNT = len(IN_CHARS)
 OUT_CHARS = [char for char in string.ascii_letters] + ['\'', '-'] + [EOS, PAD]
 OUT_COUNT = len(OUT_CHARS)
@@ -62,8 +73,10 @@ def train(x: str):
     batch_sz = len(x)
     max_len = len(max(x, key=len)) + 1  # +1 for EOS xor SOS
 
-    src_x = list(map(lambda s: [SOS] + [char for char in s] + [PAD] * ((max_len - len(s)) - 1), x))
-    trg_x = list(map(lambda s: [char for char in s] + [EOS] + [PAD] * ((max_len - len(s)) - 1), x))
+    src_x = list(
+        map(lambda s: [SOS] + [char for char in s] + [PAD] * ((max_len - len(s)) - 1), x))
+    trg_x = list(map(lambda s: [char for char in s] +
+                     [EOS] + [PAD] * ((max_len - len(s)) - 1), x))
 
     src = indexTensor(src_x, max_len, IN_CHARS).to(DEVICE)
     trg = targetsTensor(trg_x, max_len, OUT_CHARS).to(DEVICE)
@@ -105,7 +118,8 @@ def iter_train(dl: DataLoader, path: str = "Checkpoints/", print_every: int = PR
                 total_loss = 0
                 plot_losses(all_losses, x_label=f"Iteration of Batch Size: {BATCH_SZ}", y_label="NLLosss",
                             filename=NAME)
-                torch.save({'weights': lstm.state_dict()}, os.path.join(f"{path}{NAME}.path.tar"))
+                torch.save({'weights': lstm.state_dict()},
+                           os.path.join(f"{path}{NAME}.path.tar"))
 
 
 def sample():
@@ -119,7 +133,8 @@ def sample():
         for i in range(MAX_LENGTH):
             lstm_probs, lstm_hidden = lstm(lstm_input[0], lstm_hidden)
             lstm_probs = torch.softmax(lstm_probs, dim=2)
-            sample = torch.distributions.categorical.Categorical(lstm_probs).sample()
+            sample = torch.distributions.categorical.Categorical(
+                lstm_probs).sample()
             sample = sample[0]
             char = OUT_CHARS[sample]
 
