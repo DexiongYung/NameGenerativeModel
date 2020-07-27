42e8be60ac3c36f1c63903bdf51dab61798d3a62
Dylan Yung <YungDexiong@hotmail.com>
2020-07-27 05:10:31 -0700

    changed params

--- Datasets/NameCateDL.py
+++ Datasets/NameCateDL.py
@@ -3,19 +3,19 @@ from pandas import DataFrame
 
 
 class NameCategoricalDataLoader():
-    def __init__(self, df: DataFrame, batch_sz: int, name_header: str = 'name', probs_header: str = 'probs'):
+    def __init__(self, df: DataFrame, name_header: str = 'name', probs_header: str = 'probs'):
         categories = torch.FloatTensor(df[probs_header].tolist())
         self.distribution = categories
         self.data_frame = df[name_header]
-        self.batch_sz = batch_sz
         self.name_hdr = name_header
         self.probs_hdr = probs_header
 
-    def sample(self):
+    def sample(self, batch_sz: int):
         samples = []
 
-        for i in range(self.batch_sz):
-            sample = torch.distributions.Categorical(self.distribution).sample()
+        for i in range(batch_sz):
+            sample = torch.distributions.Categorical(
+                self.distribution).sample()
             samples.append(self.data_frame.iloc[sample.item()])
 
         return samples
